9b0d653ed6d8bc0ef4c7fd6c3c345482a468f1c6
Emily Znamierowski <emilyannz@gmail.com>
2016-04-27 19:02:06 -0500

    Added new lists

--- app/lists/controller.js
+++ app/lists/controller.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  saveList(attr) {
+    const list = this.store.createRecord(`list`, attr);
+
+    list.save().then(() => {
+      this.transitionToRoute(`lists.details`, list.id);
+    });
+  },
 });

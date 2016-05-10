08ecc9fe0ff8f3f2eedbf44d4ba22d5d9840ca0a
Emily Znamierowski <emilyannz@gmail.com>
2016-05-10 11:04:04 -0500

    Fixed adapter

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,5 +1,5 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
 export default JSONAPIAdapter.extend({
-  host: `https://agile-woodland-21035.herokuapp.com/`,
+  host: `https://agile-woodland-21035.herokuapp.com`,
 });
